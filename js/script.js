16b82d40f475b195c8d3fbb21c8dfcbd78250fde
Vinícius Rodrigues Godoy dos Santos <v.godoyrodrigues@gmail.com>
2021-06-07 19:00:03 -0300

    Refatora o javascript e melhora comentários

--- js/script.js
+++ js/script.js
@@ -1,23 +1,25 @@
+// Variáveis para montar e manipular o canvas
 let canvas = document.getElementById("snake"); 
 let context = canvas.getContext("2d");
-let box = 32; // Tamanho padrão de cada "caixa" do jogo
-let pontuacao = 0;
-let pontuacaoMax = 0;
-let directionBeforePause;
-let pause = false;
-let check;
-let change = 0;
+const box = 32; // Tamanho padrão de cada "caixa" do jogo
 let snake = [];
-// Inicializa a cobra no meio do canvas
-snake[0] = {
+snake[0] = { // Inicializa o array da cobra no meio do canvas
     x: 8 * box,
     y: 8 * box
 }
-let direction;
-let food = {
+let direction; // Direção da cobra
+let food = { // Inicializa a comida em algum ponto do canvas
     x: Math.floor(Math.random() * 15 + 1) * box,
     y: Math.floor(Math.random() * 15 + 1) * box
 }
+// Variáveis de pontuação e tema do site
+let score = 0;
+let highscore = 0;
+let change = 0;
+// Variáveis de outras funções do jogo
+let directionBeforePause;
+let pause = false;
+let check;
 
 /* Funções da página */
 function changeTheme(){
@@ -36,33 +38,33 @@ function changeTheme(){
     }
 }
 
-/* Funções do jogo da Cobrinha */
-function criarBG(){
-    context.fillStyle = "lightgreen"; // Define a cor do preenchimento do canvas como verde-claro
-    context.fillRect(0, 0, 16 * box, 16 * box); // Preenche o canvas desde tal coordenadas (0, 0) mais tantos números (16 * box, 16 * box) em verde-claro
+function updateScore(){ // Atualiza a pontuação
+    document.getElementById("pontuacao").innerHTML = "Pontuação: " + score; 
+    document.getElementById("pontuacaoMax").innerHTML = "Pontuação Máx: " + highscore; 
 }
 
-function criarCobrinha(){
+/* Funções do jogo da Cobrinha */
+function drawBG(){ // Desenha o canvas completo em verde a cada execução
+    context.fillStyle = "lightgreen";
+    context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+function drawSnake(){ // Desenha a cobra a cada execução
     for(i = 0; i < snake.length; i++){
-        context.fillStyle = "green"; //Define a cor do preenchimento da cobrinha como verde
-        context.fillRect(snake[i].x, snake[i].y, box, box); //Preenche o canvas desde tal coordenadas (x e y da cobrinha) mais tantos números (box, box) em verde
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-function drawFood(){
-    context.fillStyle = "red"; //Define a cor do preenchimento da comida como vermelho
-    context.fillRect(food.x, food.y, box, box); //Preenche o canvas desde tal coordenadas (x e y da comida) mais tantos números (box, box) em vermelho
+function drawFood(){ // Desenha a comida a cada execução
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
-function updateScore(){
-    document.getElementById("pontuacao").innerHTML = "Pontuação: " + pontuacao; 
-    document.getElementById("pontuacaoMax").innerHTML = "Pontuação Máx: " + pontuacaoMax; 
-}
-
-function endGame(){
-    if(pontuacaoMax < pontuacao) pontuacaoMax = pontuacao; 
-    alert("Game Over :( | Pontuação: " + pontuacao + " | Pontuação Máxima: " + pontuacaoMax);
-    pontuacao = 0;
+function endGame(){ // Encerra o jogo e reinicia todas as configurações necessárias
+    if(highscore < score) highscore = score; 
+    alert("Game Over :( | Pontuação: " + score + " | Pontuação Máxima: " + highscore);
+    score = 0;
     direction = 0;
     snake.length = 1;
     snake[0] = {
@@ -71,7 +73,16 @@ function endGame(){
     }
 }
 
-document.addEventListener('keydown', update);
+function checkFood(){ // Função pra checar se a comida não está sendo criada dentro da cobra
+    do{
+        check = 0;
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+        for(i = 0; i < snake.length; i++){
+            if(food.x == snake[i].x && food.y == snake[i].y){check++;}
+        }
+    }while(check);
+}
 
 function update(event){
     if(event.keyCode == 37 && direction != "right") direction = "left";
@@ -88,56 +99,56 @@ function update(event){
     } 
 }
 
-
-function iniciarJogo(){
+function runGame(){
+    // Primeiro verifica se o jogo está pausado para rodá-lo
     if(pause == false){
-        //If statements pra definir o loop da cobrinha no mapa
+        // Condicionais pra definir o teleporte da cobrinha nas extremidades no mapa
         if(snake[0].x > 15 * box) snake[0].x = 0; 
         if(snake[0].x < 0) snake[0].x = 16 * box;
         if(snake[0].y > 15 * box) snake[0].y = 0;
         if(snake[0].y < 0) snake[0].y = 16 * box;
 
+        // Verifica se a cobrinha está colidindo com o próprio corpo e encerra o jogo se estiver
         for(i = 1; i < snake.length; i++){
             if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
                 endGame();
             }
         }
 
-        criarBG();
-        criarCobrinha();
+        // Roda todas as funções essenciais pro jogo
+        drawBG();
+        drawSnake();
         drawFood();
         updateScore();
 
+        // Define as variáveis com o valor da cabeça da cobrinha pra incrementá-la
         let snakeX = snake[0].x;
         let snakeY = snake[0].y;
 
-        //Adiciona uma caixa pra direção que está indo
+        // Adiciona uma caixa pra direção que está indo
         if(direction == "right") snakeX += box; 
         if(direction == "left") snakeX -= box;
         if(direction == "up") snakeY -= box;
         if(direction == "down") snakeY += box;
 
-        if(snakeX != food.x || snakeY != food.y){ //Exclui a última caixa se não tiver pegado comida
+        // Exclui a última caixa se não tiver pegado comida
+        if(snakeX != food.x || snakeY != food.y){ 
             snake.pop();
         }else{
-            do{ // Se tiver pegado comida aleatoriza a comida até ela não estar dentro da cobra
-                check = 0;
-                food.x = Math.floor(Math.random() * 15 + 1) * box;
-                food.y = Math.floor(Math.random() * 15 + 1) * box;
-                for(i = 0; i < snake.length; i++){
-                    if(food.x == snake[i].x && food.y == snake[i].y){check++;}
-                }
-            }while(check);
-            pontuacao++;
+            // Se tiver pegado comida, aleatoriza a comida até ela não estar dentro da cobra
+            checkFood();
+            score++;
         }
 
+        // Cria uma variável pra passar o valor da nova cabeça pro array
         let newHead = { 
             x: snakeX,
             y: snakeY
         }
 
-        snake.unshift(newHead); //Define a última posição calculada como a cabeça da cobra
+        snake.unshift(newHead); // Adiciona na primeira posição a nova cabeça calculada
     }
 }
 
-let jogo = setInterval(iniciarJogo, 100); //Roda a função do jogo a cada 100 milisegundos
+document.addEventListener('keydown', update); // Escuta as teclas clicadas
+let game = setInterval(runGame, 100); //Roda a função do jogo a cada 100 milisegundos
