81545fb4ea0c459436cec9adde839264812fb5ce
Vinícius Godoy <v.godoyrodrigues@gmail.com>
2025-09-29 22:28:56 -0300

    refactor: remove magic numbers and improve code

--- js/script.js
+++ js/script.js
@@ -1,159 +1,180 @@
+// Game Constants
+const BOX_SIZE_IN_PX = 32;
+const GRID_SIZE = 16;
+
+// Directions
+const NONE = 0;
+const LEFT = 1;
+const RIGHT = 2;
+const UP = 3;
+const DOWN = 4;
+
+// Event KeyCodes
+const LEFT_ARROW = 37;
+const UP_ARROW = 38;
+const RIGHT_ARROW = 39;
+const DOWN_ARROW = 40;
+const SPACE_BAR = 32;
+
 const canvas = document.getElementById("snake"); 
 const context = canvas.getContext("2d");
-let directionBuffer;
-const box = 32;
-const snake = [];
-snake[0] = {
-    x: 8 * box,
-    y: 8 * box
-}
-let direction;
-const food = {
-    x: Math.floor(Math.random() * 15 + 1) * box,
-    y: Math.floor(Math.random() * 15 + 1) * box
-}
-let score = 0;
-let highscore;
-if(getHighscore() == null || getHighscore() == "null"){ 
-    highscore = 0;
-}else{
-    highscore = getHighscore();
-}
-let directionBeforePause;
-let pause = false;
-let check;
 
-function updateScore(){
+let score = 0;
+let highscore = 0;
+let direction = NONE;
+let directionBuffer;
+let directionBeforePause;
+let isPaused = false;
+const snake = [];
+
+snake[0] = {
+    x: (GRID_SIZE / 2) * BOX_SIZE_IN_PX,
+    y: (GRID_SIZE / 2) * BOX_SIZE_IN_PX
+}
+const food = {
+    x: Math.floor(Math.random() * (GRID_SIZE - 1) + 1) * BOX_SIZE_IN_PX,
+    y: Math.floor(Math.random() * (GRID_SIZE - 1) + 1) * BOX_SIZE_IN_PX
+}
+
+function updateScore() {
     document.getElementById("pontuacao").innerHTML = "Pontuação: " + score; 
     document.getElementById("pontuacaoMax").innerHTML = "Pontuação Máx: " + highscore; 
 }
 
-function saveHighscore(hs){
-    localStorage.setItem('highscore', hs);
+function saveHighscore(newHighscore) {
+    if (typeof +newHighscore === 'number') localStorage.setItem('highscore', newHighscore);
 }
 
-function getHighscore(){
-    let hs = localStorage.getItem('highscore');
-    return hs;
+function getHighscore() {
+    const highscore = localStorage.getItem('highscore');
+    if (typeof +highscore === 'number') return +highscore;
+    return null;
 }
 
-function drawBackground(){
+function drawBackground() {
     context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 16 * box, 16 * box);
+    context.fillRect(0, 0, GRID_SIZE * BOX_SIZE_IN_PX, GRID_SIZE * BOX_SIZE_IN_PX);
 }
 
-function drawSnake(){
-    for(i = 0; i < snake.length; i++){
+function drawSnake() {
+    for (i = 0; i < snake.length; i++) {
         context.fillStyle = "green";
-        context.fillRect(snake[i].x, snake[i].y, box, box);
+        context.fillRect(snake[i].x, snake[i].y, BOX_SIZE_IN_PX, BOX_SIZE_IN_PX);
     }
 }
 
-function drawFood(){
+function drawFood() {
     context.fillStyle = "red";
-    context.fillRect(food.x, food.y, box, box);
+    context.fillRect(food.x, food.y, BOX_SIZE_IN_PX, BOX_SIZE_IN_PX);
 }
 
-function endGame(){
-    if(highscore < score){
+function endGame() {
+    if (score > highscore) {
         highscore = score;
-        saveHighscore(highscore)
-    } 
+        saveHighscore(highscore);
+    }
     alert("Game Over :( | Pontuação: " + score + " | Pontuação Máxima: " + highscore);
+
+    direction = NONE;
+    directionBuffer = NONE;
     score = 0;
-    direction = 0;
     snake.length = 1;
     snake[0] = {
-        x: 8 * box,
-        y: 8 * box
-    }
+        x: 8 * BOX_SIZE_IN_PX,
+        y: 8 * BOX_SIZE_IN_PX
+    };
 }
 
-function winGame(){
-    if(snake.length == 256){
+function winGame() {
+    if (snake.length === GRID_SIZE * GRID_SIZE) {
         alert("PARABÉNS!!! VOCÊ GANHOU O JOGO!");
         highscore = score;
         saveHighscore(highscore);
         
         score = 0;
-        direction = 0;
+        direction = NONE;
+        directionBuffer = NONE;
         snake.length = 1;
         snake[0] = {
-        x: 8 * box,
-        y: 8 * box
+            x: 8 * BOX_SIZE_IN_PX,
+            y: 8 * BOX_SIZE_IN_PX
         }
     }
 }
 
-function checkFood(){
-    do{
-        check = 0;
-        food.x = Math.floor(Math.random() * 15 + 1) * box;
-        food.y = Math.floor(Math.random() * 15 + 1) * box;
-        for(i = 0; i < snake.length; i++){
-            if(food.x == snake[i].x && food.y == snake[i].y){check++;}
-        }
-    }while(check);
+function spawnFood() {
+    let isFoodInsideSnake = false;
+
+    do {
+        isFoodInsideSnake = false;
+        food.x = Math.floor(Math.random() * 15 + 1) * BOX_SIZE_IN_PX;
+        food.y = Math.floor(Math.random() * 15 + 1) * BOX_SIZE_IN_PX;
+        isFoodInsideSnake = snake.some(segment => segment.x === food.x && segment.y === food.y);
+    } while (isFoodInsideSnake);
 }
 
-function handleKeydown(event){
-    if(event.keyCode == 37 && direction != "right") directionBuffer = "left";
-    if(event.keyCode == 38 && direction != "down") directionBuffer = "up";
-    if(event.keyCode == 39 && direction != "left") directionBuffer = "right";
-    if(event.keyCode == 40 && direction != "up") directionBuffer = "down";
-    if(event.keyCode == 32) {
-        if(pause == false){
-            directionBeforePause = direction;
-            direction = 0; pause = true;
-        }else{
-            direction = directionBeforePause; pause = false;
-        }
+function handleKeydown(event) {
+    if (event.keyCode === LEFT_ARROW && direction !== RIGHT) directionBuffer = LEFT;
+    if (event.keyCode === UP_ARROW && direction !== DOWN) directionBuffer = UP;
+    if (event.keyCode === RIGHT_ARROW && direction !== LEFT) directionBuffer = RIGHT;
+    if (event.keyCode === DOWN_ARROW && direction !== UP) directionBuffer = DOWN;
+    if (event.keyCode === SPACE_BAR) {
+        directionBeforePause = isPaused ? NONE : direction;
+        direction = isPaused ? directionBeforePause : NONE;
+        isPaused = !isPaused;
     } 
 }
 
-function gameTick(){
-    if(pause == false){
-        if(snake[0].x > 15 * box) snake[0].x = 0; 
-        if(snake[0].x < 0) snake[0].x = 16 * box;
-        if(snake[0].y > 15 * box) snake[0].y = 0;
-        if(snake[0].y < 0) snake[0].y = 16 * box;
+function gameTick() {
+    if (isPaused) return;
 
-        for(i = 1; i < snake.length; i++){
-            if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
-                endGame();
-            }
-        }
+    if (snake[0].x > 15 * BOX_SIZE_IN_PX) snake[0].x = 0; 
+    if (snake[0].x < 0) snake[0].x = GRID_SIZE * BOX_SIZE_IN_PX;
+    if (snake[0].y > 15 * BOX_SIZE_IN_PX) snake[0].y = 0;
+    if (snake[0].y < 0) snake[0].y = GRID_SIZE * BOX_SIZE_IN_PX;
 
-        drawBackground();
-        drawSnake();
-        drawFood();
-        updateScore();
-
-        let snakeX = snake[0].x;
-        let snakeY = snake[0].y;
-
-        if(directionBuffer) direction = directionBuffer;
-
-        if(direction == "right") snakeX += box; 
-        if(direction == "left") snakeX -= box;
-        if(direction == "up") snakeY -= box;
-        if(direction == "down") snakeY += box;
-
-        if(snakeX != food.x || snakeY != food.y){ 
-            snake.pop();
-        }else{
-            checkFood();
-            score++;
-        }
-
-        let newHead = { 
-            x: snakeX,
-            y: snakeY
-        }
-
-        snake.unshift(newHead);
+    for (i = 1; i < snake.length; i++) {
+        const isSelfColliding = snake[0].x === snake[i].x && snake[0].y === snake[i].y;
+        if (isSelfColliding) return endGame();
     }
+
+    drawBackground();
+    drawSnake();
+    drawFood();
+    updateScore();
+
+    let snakeHeadX = snake[0].x;
+    let snakeHeadY = snake[0].y;
+
+    if (directionBuffer) direction = directionBuffer;
+
+    if (direction === LEFT) snakeHeadX -= BOX_SIZE_IN_PX;
+    if (direction === RIGHT) snakeHeadX += BOX_SIZE_IN_PX;
+    if (direction === UP) snakeHeadY -= BOX_SIZE_IN_PX;
+    if (direction === DOWN) snakeHeadY += BOX_SIZE_IN_PX;
+
+    const isCollidingWithFood = snakeHeadX === food.x && snakeHeadY === food.y;
+
+    if (isCollidingWithFood) {
+        score++;
+        spawnFood();
+    } else {
+        snake.pop();
+    }
+
+    const newHead = {
+        x: snakeHeadX,
+        y: snakeHeadY
+    }
+
+    snake.unshift(newHead);
 }
 
-document.addEventListener('keydown', handleKeydown);
-let game = setInterval(gameTick, 100);
+function main() {
+    if(getHighscore()) highscore = getHighscore();
+    document.addEventListener('keydown', handleKeydown);
+    let game = setInterval(gameTick, 100);
+}
+
+main();
+
